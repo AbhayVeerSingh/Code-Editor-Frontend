78e724e56ba3fda1a198de5cc0354313427db7f0
Abhay Veer Singh <avs.ahlawat@gmail.com>
2024-10-15 21:13:47 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -94,8 +94,8 @@ function Login(props) {
                 </div>
                 <div className='form-info'>
                     <p className="text-danger">{message}</p>
-                    <h1>Project Developer: Abhishek Sharma</h1>
-                    <h2>Contact: abhiatriat2004@gmail.com</h2>
+                    <h1>Project Developer: Abhay veer singh</h1>
+                    <h2>Contact: avs.ahlawat@gmail.com</h2>
                 </div>
             </form>
         </div>
